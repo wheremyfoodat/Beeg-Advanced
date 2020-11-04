7341df8f4782c607e26243976859d7901d7a318e
wheremyfoodat <gponiris2004@gmail.com>
2020-11-04 20:13:30 +0200

    Revert "Made bad decisions"
    This reverts commit b59d9f33191ac9c10435b9655914c062cdaaffbe.

--- src/src/ARM/mod.rs
+++
@@ -1,8 +0,0 @@
-mod arm;
-mod arm_swaps;
-mod arm_branches;
-mod arm_loadsStores;
-mod arm_PSRTransfers;
-mod arm_dataProcessing;
-mod arm_multiplications;
-mod arm_loadStoreMultiple;
--- src/src/cartridge.rs
+++
@@ -1,5 +0,0 @@
-use crate::bus::Bus;
-
-enum save_types {
-    
-}
